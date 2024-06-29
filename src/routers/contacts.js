386b2c40f25b4209e6f6354f6bd7a791b4c2184b
MariiaO28 <idg.masha@gmail.com>
2024-06-29 14:42:58 +0300

    Update contacts.js

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,10 +2,10 @@ import { Router } from 'express';
 import { getAllContactsController, getContactByIdController } from '../controllers/contacts.js';
 import ctrlWrapper from '../utils/ctrlWrapper.js';
 
-const router = Router();
+const contactsRouter = Router();
 
-router.get('/contacts', ctrlWrapper(getAllContactsController));
+contactsRouter.get('/contacts', ctrlWrapper(getAllContactsController));
 
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+contactsRouter.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
 
-export default router;
+export default contactsRouter;
